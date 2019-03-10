41c0002afc261a0632f27cc4cb9c3ab7c0bf6aa6
Marcin Pleban <pleban.marcin@gmail.com>
2019-03-10 23:47:51 +0100

    create validate email

--- Media/JS/Script.js
+++ Media/JS/Script.js
@@ -1,14 +1,4 @@
-function validateForm() {
-    var x = document.forms["contact-form"]["email"].value;
-    
-    if (x.trim() == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  }
-
-  function validateForms()
-{
+function validateForms(){
   var fields = ["name", "email", "message"]
 
   var i, l = fields.length;
@@ -16,9 +6,29 @@ function validateForm() {
   for (i = 0; i < l; i++) {
     fieldname = fields[i];
     if (document.forms["contact-form"][fieldname].value.trim() === "") {
-
-    return document.getElementById("contact-btn").disabled = true;      
+        return document.getElementById("contact-btn").disabled = true;      
     }
   }
   document.getElementById("contact-btn").disabled = false;
+  document.getElementById("button-id").classList.replace("error", "no-error");
+  document.getElementById("button-id").innerHTML = "";
+}
+
+function validateInputEmail(){
+    var inpObj = document.getElementById("input-email").value.trim();
+    var regex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+    var element = document.getElementById("button-id");
+    if(inpObj.match(regex)){
+        document.getElementById("input-email").style.border = "";
+        element.innerHTML = "";
+        element.classList.replace("error", "no-error");
+        alert("Message has been send!");
+        
+    }else{
+        document.getElementById("input-email").style.border = "3px solid #ff0000";
+        element.classList.add("error");
+        element.innerHTML = "Change your input data!";
+        document.getElementById("contact-btn").disabled = true;
+        return false;
+    }
 }
