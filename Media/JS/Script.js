71bb3d6f5cdf044cf9e7a63b198b3ac8bd5fca41
plebanm <pleban.marcin@gmail.com>
2019-03-11 21:14:55 +0100

    refactor validate input contact message

--- Media/JS/Script.js
+++ Media/JS/Script.js
@@ -1,12 +1,12 @@
 function validateForms(){
-  var fields = ["name", "email", "message"]
+  let fields = ["inputName", "inputEmail", "inputMessage"];
 
-  var i, l = fields.length;
-  var fieldname;
-  for (i = 0; i < l; i++) {
-    fieldname = fields[i];
-    if (document.forms["contact-form"][fieldname].value.trim() === "") {
-        return document.getElementById("contact-btn").disabled = true;      
+  let l = fields.length;
+  for (let i = 0; i < l; i++) {
+    console.log(fields[i]);
+    let elementValue = document.getElementById(fields[i]).value;
+    if (elementValue === "") {
+        return document.getElementById("contact-btn").disabled = true;
     }
   }
   document.getElementById("contact-btn").disabled = false;
@@ -15,20 +15,30 @@ function validateForms(){
 }
 
 function validateInputEmail(){
-    var inpObj = document.getElementById("input-email").value.trim();
-    var regex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
-    var element = document.getElementById("button-id");
-    if(inpObj.match(regex)){
-        document.getElementById("input-email").style.border = "";
-        element.innerHTML = "";
-        element.classList.replace("error", "no-error");
-        alert("Message has been send!");
-        
-    }else{
-        document.getElementById("input-email").style.border = "3px solid #ff0000";
-        element.classList.add("error");
-        element.innerHTML = "Change your input data!";
-        document.getElementById("contact-btn").disabled = true;
-        return false;
+    let fields = ["inputName", "inputEmail"];
+    let regex = ["\\b[A-Z][a-z]* [A-Z][a-z]*( [A-Z])?\\b", "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"];
+    let count=0;
+
+    for (let i = 0; i < fields.length; i++) {
+        let inpObj = document.getElementById(fields[i]).value.trim();
+        let element = document.getElementById("button-id");
+        if(inpObj.match(regex[i])){
+            document.getElementById(fields[i]).style.border = "";
+            element.innerHTML = "";
+            element.classList.replace("error", "no-error");
+            count++;
+            if (count===2){
+                alert("Message has been send!");
+            }
+        }else{
+            element.classList.add("error");
+            document.getElementById(fields[i]).style.border = "3px solid #ff0000";
+            element.innerHTML = "Change your input data!";
+            document.getElementById("contact-btn").disabled = true;
+        }
     }
+
+
+
+
 }
